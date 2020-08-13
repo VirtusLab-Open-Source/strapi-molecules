24b6e8a407992fb4deaefc0a322208196a6e2e76
Mateusz Strąg <mateusz.strag@tesco.com>
2020-08-13 11:48:34 +0200

    feat: add prettier and commitlint

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "always",
+};
