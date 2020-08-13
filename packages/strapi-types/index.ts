0e75fdc75917a24b64b5e0266fbbf1ccc5446998
Mateusz Strąg <mateusz.strag@tesco.com>
2020-08-13 14:02:44 +0200

    feat: add strapi typings and ts-jest

---
+++ packages/strapi-types/index.ts
@@ -0,0 +1,27 @@
+type ComponentAttribute = {
+  type: "component";
+  repeatable: boolean;
+  component: string;
+};
+type PrimitiveTypeAttribute = {
+  type: string;
+};
+
+type ModelAttribute = PrimitiveTypeAttribute | ComponentAttribute;
+
+type BaseModel = {
+  options: {
+    [key: string]: any;
+  };
+  allAttributes: {
+    [key: string]: ModelAttribute;
+  };
+};
+
+type ComponentModel = BaseModel & {};
+
+export type Strapi = {
+  components: {
+    [key: string]: ComponentModel;
+  };
+};
