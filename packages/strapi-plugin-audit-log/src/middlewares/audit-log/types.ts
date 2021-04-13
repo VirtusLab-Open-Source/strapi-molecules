9cca0d2af281f2c4efea7930cbfb35e18b76be28
Maksymilian Pamuła <maksymilian.pamula@gmail.com>
2021-04-13 20:06:06 +0200

    fix: audit log for new strapi version
    Signed-off-by: Maksymilian Pamuła <maksymilian.pamula@gmail.com>

--- packages/strapi-plugin-audit-log/src/middlewares/audit-log/types.ts
+++ packages/strapi-plugin-audit-log/src/middlewares/audit-log/types.ts
@@ -1,29 +1,36 @@
 import { Context } from 'koa';
 import { Base } from '../../lib/services/Base';
 
-export type RequestMethod = 'DELETE' | 'GET' | 'HEAD' | 'PATCH' | 'POST' | 'PUT';
-export type SupportMethods = Exclude<RequestMethod, 'GET' | 'HEAD' | 'PATCH'>
+export type RequestMethod =
+  | 'DELETE'
+  | 'GET'
+  | 'HEAD'
+  | 'PATCH'
+  | 'POST'
+  | 'PUT';
+export type SupportMethods = Exclude<RequestMethod, 'GET' | 'HEAD' | 'PATCH'>;
 export type User = {
   readonly id: number | string;
   readonly firstname: string;
   readonly lastname: string;
   readonly username: string;
   readonly email: string;
-}
+};
 
 export type KoaContext<T = any> = Context & {
-  auditLog?: Base<T>
+  auditLog?: Base<T>;
   method: SupportMethods;
-}
+};
 export type ServiceConfig<T = any> = {
   readonly pluginName: string;
   readonly serviceName: string;
-  readonly Class?: Base<T>
+  readonly controllerName: string;
+  readonly Class?: Base<T>;
   readonly decorate?: boolean;
-}
+};
 
 export type AuditLogMiddleware = {
-  readonly map: ServiceConfig[]
-  readonly methods?: SupportMethods[]
-  readonly exclude?: string[]
-}
+  readonly map: ServiceConfig[];
+  readonly methods?: SupportMethods[];
+  readonly exclude?: string[];
+};
