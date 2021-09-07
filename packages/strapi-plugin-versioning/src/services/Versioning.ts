0d5e94c7c127a380ab643ed5854724a191309c03
Maksymilian Pamuła <maksymilian.pamula@gmail.com>
2021-09-07 20:15:47 +0200

    fix: add support for single type - handle when single type not exist
    Signed-off-by: Maksymilian Pamuła <maksymilian.pamula@gmail.com>

--- packages/strapi-plugin-versioning/src/services/Versioning.ts
+++ packages/strapi-plugin-versioning/src/services/Versioning.ts
@@ -3,6 +3,7 @@ import {
   isObject,
   noop,
 } from 'lodash';
+import { GlobalQueryResult } from 'strapi-types';
 
 type Entity = any;
 type Data = Array<Entity>;
@@ -27,8 +28,8 @@ const findUid = (ctx: Context): string =>
   ctx.params.model
     ? global.strapi.db.getModel(ctx.params.model).uid
     : Object.values(global.strapi.models).find(
-        (el) => el.collectionName == ctx.url.split('/')[1],
-      )!.uid;
+      (el) => el.collectionName == ctx.url.split('/')[1],
+    )!.uid;
 
 const saveDataInDB = async (
   data: Data,
@@ -54,26 +55,36 @@ const getVersionsForAllConentTypes = async () => {
   return knexQueryBuilder;
 };
 
-const getVersionsForEntity = async (contentType: string, id: string) => {
-  const model = global.strapi.query(contentType);
-  const knexQueryBuilder = global.strapi.connections.default('versions');
-  knexQueryBuilder
-    .select();
-  if (model) {
-    const { model: { kind } } = model;
-    if (kind === 'singleType') {
-      const { id: singleTypeId } = await model.findOne({});
-      knexQueryBuilder
-        .where({ content_type: contentType, entity_id: singleTypeId });
-    } else {
-      knexQueryBuilder
-        .where({ content_type: contentType, entity_id: id });
-    }
-  }
-  knexQueryBuilder
+const isSingleType = (kind: 'singleType' | 'collectionType') => kind === 'singleType';
+
+const knexBuildQuery = (contentType: string, id: string): Promise<any[]> => {
+  const builder = global.strapi.connections.default('versions');
+  builder
+    .select()
+    .where({ content_type: contentType, entity_id: id })
     .returning('*')
     .toString();
-  return knexQueryBuilder;
+  return builder;
+};
+
+const getVersionsForSingleType = async (model: GlobalQueryResult, contentType: string) => {
+  const entity = await model.findOne({});
+  if (entity) {
+    return knexBuildQuery(contentType, entity.id)
+  }
+  return Promise.resolve([]);
+};
+
+const getVersionsForEntity = async (contentType: string, id: string) => {
+  const model = global.strapi.query(contentType);
+  if (model) {
+    const { model: { kind } } = model;
+    if (isSingleType(kind)) {
+      return getVersionsForSingleType(model, contentType);
+    }
+    return knexBuildQuery(contentType, id);
+  }
+  return (<any>global.strapi).errors.badRequest('Content not exists');
 };
 
 module.exports = {
